c55c0df9159e340a30d20f79ca16eedcae23b170
feng <792393483@qq.com>
2019-05-13 18:04:01 +0800

    添加字符串处理方法

--- library/src/main/java/com/frey/treasure/text/PatternText.java
+++ library/src/main/java/com/frey/treasure/text/PatternText.java
@@ -1,4 +1,24 @@
 package com.frey.treasure.text;
 
 public class PatternText {
+    /**
+     * 手机号码
+     */
+    public static final String PHONE_NUMBER = "^((17[0-9])|(14[0-9])|(13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+    /**
+     * 邮箱
+     */
+    public static final String EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+    /**
+     * 一个或多个中文字符
+     */
+    public static final String CHINESE = "[\\u4e00-\\u9fa5]+";
+    /**
+     * 18位身份证
+     */
+    public static final String ID_CARD_18 = "[1-8]\\d{5}[1-9]\\d{3}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[01])(\\d{4}|\\d{3}X)";
+    /**
+     * 15位身份证
+     */
+    public static final String ID_CARD_15 = "[1-8]\\d{5}\\d{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[01])(\\d{3})";
 }
--- library/src/main/java/com/frey/treasure/text/StringUtil.java
+++ library/src/main/java/com/frey/treasure/text/StringUtil.java
@@ -1,5 +1,10 @@
 package com.frey.treasure.text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
     public static boolean isBlank(String text) {
         return text == null || text.trim().equals("");
@@ -7,5 +12,59 @@ public class StringUtil {
 
     public static boolean isNotBlank(String text) {
         return !isBlank(text);
+    }
+
+    public static List<String> getMatchTextList(String input, String regex) {
+        List<String> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            list.add(matcher.group());
+        }
+        return list;
+    }
+
+    public static String getFirstMatchText(String input, String regex) {
+        List<String> list = getMatchTextList(input, regex);
+        if (list != null && list.size() > 0)
+            return list.get(0);
+        else
+            return "";
+    }
+
+    public static boolean isMatch(String input, String regex) {
+        return Pattern.matches(regex, input);
+    }
+
+    public static String extractText(String startStr, String endStr, String input) {
+        int startIndex;
+        int endIndex;
+        if (isBlank(startStr)) {
+            startIndex = 0;
+        } else {
+            int index = input.indexOf(startStr);
+            if (index < 0)
+                return "";
+
+            if (index + startStr.length() > input.length() - 1)
+                return "";
+
+            startIndex = index + startStr.length();
+        }
+
+        if (isBlank(endStr)) {
+            endIndex = input.length();
+        } else {
+            int index = input.indexOf(endStr);
+            if (index < 0)
+                return "";
+
+            endIndex = index;
+        }
+
+        if (startIndex >= endIndex)
+            return "";
+
+        return input.substring(startIndex, endIndex);
     }
 }
